5fafb9f50129aa3779ab6c76d1b87387f3946b7c
l1o22u333 <121701663+l1o22u333@users.noreply.github.com>
2025-06-23 23:01:54 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,8 @@ dependencies {
 
 // Bill of Materials (BOM) for all Compose libraries, ensures compatible versions
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation ("com.contrarywind:Android-PickerView:4.1.9")
+    implementation("com.reinhard:Android-PickerView:4.1.9") 
+    //implementation ("com.contrarywind:Android-PickerView:4.1.9")
 // Compose UI framework
     implementation("androidx.compose.ui:ui")
 
