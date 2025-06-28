c0d915c2656ba94910696583ba5358a36cf55b72
l1o22u333 <121701663+l1o22u333@users.noreply.github.com>
2025-06-28 16:32:16 +0800

    Update MyApplication.kt

--- app/src/main/java/com/wangyiheng/vcamsx/MyApplication.kt
+++ app/src/main/java/com/wangyiheng/vcamsx/MyApplication.kt
@@ -15,5 +15,34 @@ class MyApplication : Application() {
             androidContext(this@MyApplication)
             modules(appModule,networkModule)
         }
+		createGlobalVideoFolder()
+    }
+	
+    private fun createGlobalVideoFolder() {
+        // 使用一個不易衝突且語義明確的路徑
+        val videoFolderPath = "/data/local/tmp/vcamsx_video"
+        val videoFolder = File(videoFolderPath)
+
+        if (!videoFolder.exists()) {
+            Log.d("VCAMSX_ROOT", "Global folder doesn't exist. Creating with root...")
+            try {
+                // 創建一個 su 進程來執行多條命令
+                val process = Runtime.getRuntime().exec("su")
+                val os = DataOutputStream(process.outputStream)
+                // 創建文件夾
+                os.writeBytes("mkdir -p $videoFolderPath\n")
+                // 賦予最高權限：讀、寫、執行對所有用戶開放
+                os.writeBytes("chmod 777 $videoFolderPath\n")
+                os.writeBytes("exit\n")
+                os.flush()
+                process.waitFor()
+                Log.d("VCAMSX_ROOT", "Global folder created and permissions set.")
+            } catch (e: Exception) {
+                Log.e("VCAMSX_ROOT", "Failed to create global folder with root", e)
+                // 這裡可以給用戶一個 Toast 提示，告知需要 Root 權限
+            }
+        } else {
+            Log.d("VCAMSX_ROOT", "Global folder already exists.")
+        }
     }
 }
